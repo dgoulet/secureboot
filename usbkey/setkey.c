3b1865be08ac3c2db083363a88793bf835b3542e
Stéphane Graber <stgraber@ubuntu.com>
2013-09-06 23:23:11 -0400

    usbkey: Commit a working version of setkey
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- usbkey/setkey.c
+++ usbkey/setkey.c
@@ -3,31 +3,28 @@
 
 #define BUFSIZE 2048
 
-static EFI_STATUS set_variable (CHAR16 *name, CHAR16 *data)
-{
-   EFI_STATUS  Status  = EFI_SUCCESS;
-   EFI_GUID Guid = (EFI_GUID){ 0xd719b2cb, 0x3d3a, 0x4596,
-                              {0xa3,0xbc,0xda,0xd0,0x0e,0x67,0x65,0x6f}};
-   UINTN       VarSize;
-
-   if (NULL == data) {
-       return EFI_INVALID_PARAMETER;
-   }
-
-   VarSize = (StrLen (data) + 1) * sizeof (CHAR16);
-   Status = RT->SetVariable (name,
-                            &Guid,
-                            EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS|EFI_VARIABLE_NON_VOLATILE,
-                            VarSize,
-                            data);
-   return Status;
-}
 
 EFI_STATUS efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
+    CHAR16 Data[BUFSIZE];
+    UINTN DataSize = 0;
+    UINTN Attr = EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS;
+    EFI_GUID Guid = (EFI_GUID){ 0xd719b2cb, 0x3d3a, 0x4596,
+                                    {0xa3,0xbc,0xda,0xd0,0x0e,0x67,0x65,0x6f}};
     EFI_STATUS status = EFI_SUCCESS;
 
     InitializeLib(image, systab);
-    status = set_variable(L"OurSecretVariable", L"abcdefg");
+
+    ZeroMem(Data, BUFSIZE);
+    StrCpy(Data, L"stgraber_debug: abcdef\n");
+    DataSize = StrLen(Data) * 2;
+
+    status = uefi_call_wrapper(RT->SetVariable, 5, L"bootme_flag",
+                        &Guid, Attr, DataSize, &Data);
+
+    if (status != EFI_SUCCESS)
+        Print(L"ERROR: SetVariable failed: %d\n", status);
+    else
+        Print(L"Variable set to: %s\n", Data);
 
     return status;
 }
